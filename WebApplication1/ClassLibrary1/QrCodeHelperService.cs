7120baa57524d397faf58899a89000dcf9308391
VivesPascalVanWijk <pascal.vanwijk@student.vives.be>
2026-05-07 08:50:16 +0200

    Add QR code basics (WIP)

---
+++ WebApplication1/ClassLibrary1/QrCodeHelperService.cs
@@ -0,0 +1,13 @@
+﻿using MyPlaygroundApp.Utils;
+
+namespace QRCodeGenerator.Services
+{
+    public class QrCodeHelperService
+    {
+        string url = "https://dev.to";
+
+        string file = @"C:\Users\User\Downloads\devto.png";
+
+        QRCodeHelper.GenerateToFile(url, file);
+    }
+}
